82b77750b1ce0f201a624e2c5f901d65fe8f4920
gyu85 <gyu.will85@gmail.com>
2022-01-09 00:29:49 +0900

    [220103] - route 공통 추가

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,11 +1,62 @@
-export const getPathHome = () => '/';
-export const getPathStroy = () => '/stroy';
-export const getPathBaminAd = () => '/baemin-ad';
-export const getPathRelayChannel = () => '/relay-channel';
-export const getPathKnowHow = () => '/knowhow';
-export const getPathAffiliates = () => '/affiliates';
-export const getPathGuide = () => '/guide';
-export const getPathNotice = () => '/notice';
-export const getPathCsCenter = () => '/cscenter';
-export const getPathLogin = () => '/login';
-export const getPathJoin = () => '/join';
+import { format } from 'url';
+
+const ROUTES = {
+  HOME: {
+    INDEX: '/',
+    MEMBER: '/member/{id}'
+  },
+  STORY: {
+    INDEX: '/stroy'
+  },
+  BAMINAD: {
+    INDEX: '/baemin-ad'
+  },
+  RELAYCHANNEL: {
+    INDEX: '/relay-channel'
+  },
+  KNOWHOW: {
+    INDEX: '/knowhow'
+  },
+  AFFILEATES: {
+    INDEX: '/affiliates'
+  },
+  GUIDE: {
+    INDEX: '/guide'
+  },
+  NOTICE: {
+    INDEX: '/notice'
+  },
+  CSCENTER: {
+    INDEX: '/cscenter'
+  },
+  LOGIN: {
+    INDEX: '/login'
+  },
+  JOIN: {
+    INDEX: '/join'
+  }
+};
+
+const getPath = (category, subCategory, query = {}, params = {}) => {
+  let path = `${ROUTES[category][subCategory]}${
+    query ? `${format({ query })}` : ''
+  }`;
+
+  for (const [key, value] of Object.entries(params)) {
+    path = path?.replace(new RegExp(`{${key}}`, 'g'), `${value || ''}`);
+  }
+
+  return path;
+};
+
+export const getPathHome = () => getPath('HOME', 'INDEX');
+export const getPathStroy = () => getPath('STORY', 'INDEX');
+export const getPathBaminAd = () => getPath('BAMINAD', 'INDEX');
+export const getPathRelayChannel = () => getPath('RELAYCHANNEL', 'INDEX');
+export const getPathKnowHow = () => getPath('KNOWHOW', 'INDEX');
+export const getPathAffiliates = () => getPath('AFFILEATES', 'INDEX');
+export const getPathGuide = () => getPath('GUIDE', 'INDEX');
+export const getPathNotice = () => getPath('NOTICE', 'INDEX');
+export const getPathCsCenter = () => getPath('CSCENTER', 'INDEX');
+export const getPathLogin = () => getPath('LOGIN', 'INDEX');
+export const getPathJoin = () => getPath('JOIN', 'INDEX');
