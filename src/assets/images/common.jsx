46a234bf0ee7b71393d1eee3346403e4e5c94fa8
gyu85 <gyu.will85@gmail.com>
2022-01-17 23:42:58 +0900

    [220109] - carousel  초기 작업

---
+++ src/assets/images/common.jsx
@@ -0,0 +1,4 @@
+import { ReactComponent as IcoArrowPrevt24x24 } from './svgs/common/ico-arrow-prev-24x24.svg';
+import { ReactComponent as IcoArrowNext24x24 } from './svgs/common/ico-arrow-next-24x24.svg';
+
+export { IcoArrowPrevt24x24, IcoArrowNext24x24 };
